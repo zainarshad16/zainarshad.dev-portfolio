90ecc547b79fa5a7f7a743eee4b986b0c1be812c
Zain Arshad <zainarshad110@gmail.com>
2025-11-16 14:08:07 +0500

    Update introduction text for Zain Arshad

--- app/page.tsx
+++ app/page.tsx
@@ -30,8 +30,8 @@ export default function Home() {
           >
             {<div className="info flex flex-col justify-center items-center mt-3 text-center">
               <span className="md:text-6xl text-4xl font-bold">Hi, I'm Zain Arshad</span>
-              <span className="md:text-4xl text-2xl font-bold text-gray-300 flex flex-col md:gap-13 gap-8">I am a Full Stack Engineer contributing in many Large-scale Applications.
-                <span className="text-lg font-normal">With expertise in MERN Stack, .NET, Reactjs, NEXTjs and Blockchain, I focus on delivering scalable and efficient products.</span>
+              <span className="md:text-4xl text-2xl font-bold text-gray-300 flex flex-col md:gap-13 gap-8">I am a Full Stack Developer Intern contributing to impactful and modern web applications.
+                <span className="text-lg font-normal">With expertise in MERN Stack, Reactjs and NEXTjs, I focus on delivering scalable and efficient products.</span>
               </span>
             </div>}
           </motion.div>
